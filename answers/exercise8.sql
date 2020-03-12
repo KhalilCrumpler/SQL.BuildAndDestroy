a19d191a51abdaad83c9f2cbf6e684ec2f86e195
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-12 13:44:31 -0400

    finished exercise 8

---
+++ answers/exercise8.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Students (StudentName VARCHAR(255), Address VARCHAR(255), City VARCHAR(255), PostalCode VARCHAR(255));
+INSERT INTO Students(StudentName, Address, City, PostalCode)
+VALUES('Jane Doe', '57 Union St', 'Glasglow, Scotland', 'G13RB');
